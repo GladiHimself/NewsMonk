21f2fe06f5b3e2202e48008b3aa250a902929b6a
Pranav Praveen <pranprav2001@gmail.com>
2026-06-06 23:46:41 +0530

    UI changes to make it better

--- newsapp/src/components/NavBar.js
+++ newsapp/src/components/NavBar.js
@@ -4,22 +4,22 @@ export class NavBar extends Component {
   render() {
     return (
       <div>
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">NewsMonk</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow-sm" style={{borderBottom: "2px solid #6c63ff"}}>
+  <div className="container">
+    <a className="navbar-brand fw-bold fs-4" href="/" style={{letterSpacing: "0.5px"}}>
+      📰 NewsMonk
+    </a>
+    <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-1">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link active px-3" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/about">About</a>
+          <a className="nav-link px-3" href="/about">About</a>
         </li>
-        
-    
       </ul>
     </div>
   </div>
